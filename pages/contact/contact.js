7c45c3df971801d9fa83b64548085c51e67ec3cb
Akrawit Suwansantisuk <akrawit.suw@gmail.com>
2021-05-28 18:18:59 +0700

    Update css

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -9,7 +9,7 @@ export default function Contact() {
             <h2 className={styles.title}>Akrawit Suwansantisuk</h2>
             <h2> Experience: </h2>
             <ul>
-              <li className={styles.experience}><image></image> 2017- present: Software Engineer at Agoda services </li>
+              <li className={styles.experience}> 2017- present: Software Engineer at Agoda services </li>
               <li className={styles.experience}> 2015 - 2017: Full Stack Developer at Wongnai. </li>
               <li className={styles.experience}> 2012 - 2014: Java developer at CDG systems.</li>
             </ul>
