02c40288f4b4197c2aa749ec4cffbb2a2a7f7177
Akrawit Suwansantisuk <akrawit.suw@gmail.com>
2021-06-30 13:28:16 +0700

    Change layout and add favicon

--- pages/experience/experienceCard.js
+++ pages/experience/experienceCard.js
@@ -3,16 +3,17 @@ import styles from "../../styles/Experience.module.css"
 export default function ExperienceCard(props) {
     return (
         <li className={styles.experience}>
-            <div className={styles.experienceinfo}>
-                <span className={styles.title}>{props.title} </span>
-                <span>{props.period}</span>
-                <span>{props.company}</span>
-            </div>
             <div>
                 <a href={props.url} target="_blank">
                     <img src={props.imageSource} alt={props.alt} className={styles.logo} />
                 </a>
             </div>
+            <div className={styles.experienceinfo}>
+                <span className={styles.title}>{props.title} </span>
+                <span>{props.period}</span>
+                <span>{props.company}</span>
+            </div>
+
         </li>
-        )
+    )
 };
