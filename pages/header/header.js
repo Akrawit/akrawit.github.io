f086652f44b50b643eb7eb81eeb302677765b51d
Akrawit Suwansantisuk <akrawit.suw@gmail.com>
2021-06-30 14:40:12 +0700

    Update header

--- pages/header/header.js
+++ pages/header/header.js
@@ -20,8 +20,11 @@ export default function Header() {
   return (
     <header id="head" className={styles.main} >
       <Head>
-        <title>Akrawit</title>
+        <title>Akrawit Suwansantisuk - Resume</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="description" content="This is my resume website. Build this for fun." />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200&display=swap" rel="stylesheet" />
