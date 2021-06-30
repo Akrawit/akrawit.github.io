0b206c3e9faeaf295f9d5b3cde62dbae40665128
Akrawit Suwansantisuk <akrawit.suw@gmail.com>
2021-06-30 12:04:39 +0700

    Refactor and fix height

--- pages/experience/experience.js
+++ pages/experience/experience.js
@@ -1,63 +1,48 @@
 import styles from "../../styles/Experience.module.css"
 import Slide from 'react-reveal/Slide';
 import Image from 'next/image'
+import ExperienceCard from "./ExperienceCard";
+
 
 export default function Experience() {
   return (
     <section className={styles.second}>
       <Slide left>
         <div>
-          <h3> Experience: </h3>
+          <h3> Experience </h3>
           <ul className={styles.experiencelist}>
-            <li className={styles.experience}>
-              <div className={styles.experienceinfo}>
-                <span className={styles.title}>Software Engineer </span>
-                <span>2017 - present</span>
-                <span>Agoda Services</span>
-              </div>
-              <div>
-                <a href='https://www.linkedin.com/company/agoda' target="_blank">
-                  <img src="/agoda.png" alt="agoda-logo" className={styles.logo} />
-                </a>
-              </div>
-            </li>
-            <li className={styles.experience}>
-              <div className={styles.experienceinfo}>
-                <span className={styles.title}>Full Stack Developer</span>
-                <span>2015 - 2017</span>
-                <span>Wongnai</span>
-              </div>
-              <div>
-                <a href='https://www.linkedin.com/company/wongnai.com/' target="_blank">
-                  <img src="/wongnai.png" alt="wongnai-logo" className={styles.logo} />
-                </a>
-              </div>
-
-            </li>
-            <li className={styles.experience}>
-              <div className={styles.experienceinfo}>
-                <span className={styles.title}> Java developer</span>
-                <span>2012 - 2014</span>
-                <span>CDG systems</span>
-              </div>
-              <div>
-                <a href='https://www.linkedin.com/company/cdg-systems-cdgs-/' target="_blank">
-                  <img src="/CDGS.png" alt="cdgs-logo" className={styles.logo} />
-                </a>
-              </div>
-            </li>
-            <li className={styles.experience}>
-              <div className={styles.experienceinfo}>
-                <span className={styles.title}> Intern</span>
-                <span>2010</span>
-                <span>Universiti Teknologi Malaysia</span>
-              </div>
-              <div>
-                <a href='https://www.linkedin.com/school/universiti-teknologi-malaysia/' target="_blank">
-                  <img src="/UTM.png" alt="utm-logo" className={styles.logo} />
-                </a>
-              </div>
-            </li>
+            <ExperienceCard
+              title="Software Engineer"
+              period="2017 - present"
+              company="Agoda Services"
+              url="https://www.linkedin.com/company/agoda"
+              imageSource="/agoda.png"
+              alt="agoda-logo"
+            />
+            <ExperienceCard
+              title="Full Stack Developer"
+              period="2015 - 2017"
+              company="Wongnai Media"
+              url="https://www.linkedin.com/company/wongnai.com/"
+              imageSource="/wongnai.png"
+              alt="wongnai-logo"
+            />
+            <ExperienceCard
+              title="Java developer"
+              period="2012 - 2014"
+              company="CDG systems"
+              url="https://www.linkedin.com/company/cdg-systems-cdgs-/"
+              imageSource="/CDGS.png"
+              alt="cdgs-logo"
+            />
+            <ExperienceCard
+              title="Intern"
+              period="2010"
+              company="Universiti Teknologi Malaysia"
+              url="https://www.linkedin.com/school/universiti-teknologi-malaysia/"
+              imageSource="/UTM.png"
+              alt="utm-logo"
+            />
           </ul>
         </div>
       </Slide>
