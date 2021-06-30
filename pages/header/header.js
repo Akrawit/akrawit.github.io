92af777ea33061f6d4cde8994a55f3fb92ed1094
Akrawit Suwansantisuk <akrawit.suw@gmail.com>
2021-06-30 14:29:33 +0700

    Change font family

--- pages/header/header.js
+++ pages/header/header.js
@@ -18,17 +18,20 @@ export default function Header() {
     opacity: 0.2,
   }
   return (
-    <header id="head" className={styles.main}>
+    <header id="head" className={styles.main} >
       <Head>
         <title>Akrawit</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200&display=swap" rel="stylesheet" />
       </Head>
       <main className={styles.main}>
         <section className={styles.firstSection} >
           <ParticlesBg type="cobweb" bg={true} bg={config} />
           <h1 className={styles.title}>AKRAWIT</h1>
           <img src="/profile.jpg" alt="profile-image" className={styles.profile} />
-          <h2>I'm a developer, runner, traveler.</h2>
+          <h2 className={styles.description}>I'm a developer, runner, traveler.</h2>
         </section>
       </main>
     </header>
