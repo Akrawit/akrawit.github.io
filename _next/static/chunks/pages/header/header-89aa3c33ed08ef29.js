bcf0b7dd66b3cad93988f34226044d527df8dd83
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-09-19 15:51:07 +0000

    Deploy akrawit.com to github.com/Akrawit/akrawit.github.io.git:gh-pages

---
+++ _next/static/chunks/pages/header/header-89aa3c33ed08ef29.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[990],{3170:function(n,_,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/header/header",function(){return e(4305)}])}},function(n){n.O(0,[112,814,305,774,888,179],(function(){return _=3170,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
