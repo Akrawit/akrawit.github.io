f0f65aca653a1ebdc0286fc41f173216e455708b
Akrawit Suwansantisuk <akrawit.suw@gmail.com>
2026-01-17 10:23:43 +0700

    update

---
+++ docs/_next/static/chunks/pages/header/header-35cee8c55651f405.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[990],{6401:function(n,_,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/header/header",function(){return e(3523)}])}},function(n){n.O(0,[948,489,523,888,774,179],function(){return n(n.s=6401)}),_N_E=n.O()}]);
